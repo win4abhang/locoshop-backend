a0fafe17fd17b38e980862e2704e90cdfa66091d
win4abhang <win4abhang@gmail.com>
2025-04-21 16:01:24 +0530

    Add Autocomplete Dropdown

--- routes/storeRoutes.js
+++ routes/storeRoutes.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { searchStores } = require('../controllers/storeController');
+const { searchStores, getStoreSuggestions } = require('../controllers/storeController');
 
 router.get('/search', searchStores);
+router.get('/suggestions', getStoreSuggestions);  // 👈 Add this line
 
 module.exports = router;
