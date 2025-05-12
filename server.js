38c698d747af2bc0b896bca01edff7b9db82c409
win4abhang <win4abhang@gmail.com>
2025-05-12 10:50:43 +0530

    error deprecated

--- server.js
+++ server.js
@@ -26,14 +26,11 @@ app.use("/api/stores", storeRoutes);
 
 // ✅ MongoDB connection
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB connected");
 
-    // ✅ Start server
+    // ✅ Start server using correct Render port
     const PORT = process.env.PORT || 5000;
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   })
